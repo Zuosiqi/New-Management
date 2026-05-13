4d0036a6c57f9be169e9a29da6ba2f26fcfd94c1
Zuosiqi <2924752795@qq.com>
2026-05-13 15:10:43 +0800

    feat: add employee service module and fix maven build

---
+++ employee-service/src/main/java/com/attendance/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.attendance.employeeservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employee/test")
+    public String test() {
+        return "employee-service is running";
+    }
+}
